2f3e5247eb152e62692e1dcfbd86b91a285b216d
juah3h32 <coronapablo99@gmail.com>
2026-01-04 00:59:08 -0600

    Agregando componentes de Astro

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,9 @@
 /// <reference types="astro/client" />
-/// <reference types="@vite-pwa/astro/info" />
-/// <reference types="@vite-pwa/astro/client" />
+/// <reference types="vite-plugin-pwa/info" />
+/// <reference types="vite-plugin-pwa/client" />
+
+// DEFINICIONES MANUALES DE RESPALDO
+// (Por si TypeScript no detecta las referencias automáticas)
 
 declare module 'virtual:pwa-info' {
     export const pwaInfo: {
@@ -9,4 +12,17 @@ declare module 'virtual:pwa-info' {
         linkTag: string;
       };
     };
+  }
+  
+  declare module 'virtual:pwa-register' {
+    export interface RegisterSWOptions {
+      immediate?: boolean;
+      onNeedRefresh?: () => void;
+      onOfflineReady?: () => void;
+      onRegistered?: (registration: ServiceWorkerRegistration | undefined) => void;
+      onRegisteredSW?: (swUrl: string, registration: ServiceWorkerRegistration | undefined) => void;
+      onRegisterError?: (error: any) => void;
+    }
+  
+    export function registerSW(options?: RegisterSWOptions): (reloadPage?: boolean) => Promise<void>;
   }
